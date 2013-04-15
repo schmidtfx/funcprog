0994cbe86104dba82d5306080dadff9bb1174211
Felix Schmidt <schmidtfx@gmail.com>
2013-04-15 16:19:08 +0200

    Week 4 assignment

---
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,84 @@
+package patmat
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import patmat.Huffman._
+
+@RunWith(classOf[JUnitRunner])
+class HuffmanSuite extends FunSuite {
+  trait TestTrees {
+    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+  }
+
+  test("weight of a larger tree") {
+    new TestTrees {
+      assert(weight(t1) === 5)
+    }
+  }
+
+  test("chars of a larger tree") {
+    new TestTrees {
+      assert(chars(t2) === List('a','b','d'))
+    }
+  }
+
+  test("string2chars(\"hello, world\")") {
+    assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
+  test("makeOrderedLeafList for some frequency table") {
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+  }
+
+  test("combine of some leaf list") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assert(	combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+  }
+  
+  test("combine of singleton or nil") {
+   val list1 = List(Leaf('e', 1))
+   assert(combine(list1) === list1)
+   assert(combine(Nil) === Nil)
+  }
+
+  test("decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+  
+  test("encode some text with french code") {
+    assert(encode(frenchCode)(List('h', 'u', 'f', 'f', 'm', 'a', 'n', 'e', 's', 't', 'c', 'o', 'o', 'l')) === secret)
+  }
+  
+  test("times") {
+    new TestTrees {
+      assert(times(List('a', 'b', 'a')) === List(('a', 2), ('b', 1)))
+    }
+  }
+  
+  test("decodedSecret") {
+    println(decodedSecret)
+  }
+  
+  test("quickEncode") {
+    new TestTrees {
+      assert(decode(t1, quickEncode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+  
+  test("quickEncode 2") {
+    val toEncode = decodedSecret
+    val encoded = quickEncode(frenchCode)(toEncode)
+    assert(encoded === secret)
+  }
+  
+  test("create code tree") {
+    val text=List('e', 'n', 'c', 'o', 'r', 'e', 'u', 'n', 't', 'e', 'x', 't', 'e', 't', 'r', 'e', 's', 's', 'e', 'c', 'r', 'e', 't')
+    println(createCodeTree(text))
+  }
+}
